220bc8da846f56245286f0a8cc39225f011e55d8
Ben Laurie <benl@google.com>
2015-08-23 12:57:45 +0100

    Add sync and ack and missing return.

--- arduino_proxy/arduino_proxy.ino
+++ arduino_proxy/arduino_proxy.ino
@@ -17,31 +17,28 @@ void setup()
   
   transmitter->bind(0);
   Serial.println("found a craft!");
+
+  Serial.print('Z');  // sync
   
   // TODO:  auto-arm  (throttle from 0 -> 1000 -> 0 again)
 }
 
-
-uint16_t read16() {  // assumes MSB first.
+uint16_t read16()
+{  // assumes MSB first.
   uint16_t result = Serial.read();
   result = (result << 8) + Serial.read();
+  return result;
 }
 
 void loop()
-{
+{  
   transmitter->loop();
-  //Serial.println("hello");
+
   if (Serial.available() >= 8) {
-    // This acts as a start marker, and in the common case
-    // will cause a lost byte to be eventually resynced.
-    if (Serial.read() == 255) {
-      //digitalWrite(13, LOW);
-      transmitter->setAileron(0, read16());
-      transmitter->setElevator(0, read16());
-      uint16_t th = read16();
-      transmitter->setThrottle(0, th);
-      transmitter->setRudder(0, read16());
-      Serial.print(th);
-    }
+    transmitter->setAileron(0, read16());
+    transmitter->setElevator(0, read16());
+    transmitter->setThrottle(0, read16());
+    transmitter->setRudder(0, read16());
+    Serial.print('+');  // ack
   }
 };
---
+++ arduino_proxy/test_proxy.py
@@ -0,0 +1,65 @@
+import serial
+import sys
+import time
+
+ser = serial.Serial('/dev/cu.usbserial-DC008KDX', 115200)
+
+while True:
+    x = ser.read()
+#    sys.stdout.write(x)
+    print ord(x), x
+    if x == 'Z':
+        break
+
+print "Ready!"
+
+def send(throttle, rudder, elevator, aileron):
+    echo()
+    print "t =", throttle, "r =", rudder, "e =", elevator, "a =", aileron
+    send16(aileron)
+    send16(elevator)
+    send16(throttle)
+    send16(rudder)
+    ack()
+
+def send16(n):
+    send8((n >> 8) & 0xff)
+    send8(n & 0xff)
+
+def send8(n):
+    ser.write(chr(n))
+
+def echo():
+    while ser.inWaiting():
+        t = ser.read()
+        sys.stdout.write(t)
+
+def ack():
+    while True:
+        t = ser.read()
+        sys.stdout.write(t)
+        if t == '+':
+            break
+
+MAX = 1000
+MIN = 0
+MID = 500
+
+# ARM
+send(MIN, MID, MID, MID)
+time.sleep(1)
+send(MAX, MID, MID, MID)
+time.sleep(1)
+send(MIN, MID, MID, MID)
+time.sleep(1)
+
+MAXT = MAX/4
+
+# Run
+for t in range(MAXT):
+    send(t, MID, MID, MID)
+#    time.sleep(.01)
+
+for t in range(MAXT, 0, -1):
+    send(t, MID, MID, MID)
+#    time.sleep(.01)
