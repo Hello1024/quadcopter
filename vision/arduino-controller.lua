0daa05ca652d215c55e527947e8138eed48770bf
Oliver Mattos <omattos@gmail.com>
2015-09-10 22:49:56 +0100

    Convert controller to all in one module, tidy, add logging, slow serial speed, probably other things I forgot now

--- vision/arduino-controller.lua
+++ vision/arduino-controller.lua
@@ -4,47 +4,67 @@
 -- sudo  chmod o+rw /dev/ttyU*;
 
 
+QuadConnection = {}
+QuadConnection.__index = QuadConnection
+
+function QuadConnection.create(device)
+  local this = {}
+  setmetatable(this, QuadConnection)
+  
+  this.f = S.open(device, "rdwr, noctty, nonblock")
+  local f_term = S.tcgetattr(f)
+  f_term:makeraw()
+  f_term.c_cflag = S.c.CFLAG["CS8,CREAD,CLOCAL,CSTOPB"]
+  f_term.ispeed = 9600
+  f_term.ospeed = 9600
+  this.f:tcsetattr("now", f_term)
+
+  -- Opening it resets the hardware.  Now wait for device to indicate
+  -- readyness.
+  while true do
+    str = this.f:read(nil, 1024)
+    if str then
+      print(str)
+    end
+    -- Possible race bug here, Z may not appear alone, or might appear
+    -- in other startup info.
+    if str == "Z" then
+      break
+    end
+  end
+  return this
+end
+
+function QuadConnection:SendToQuad(quadnumber, ch1, ch2, ch3, ch4)
+  write(self.f, quadnumber);
+  write(self.f, ch1);
+  write(self.f, ch2);
+  write(self.f, ch3);
+  write(self.f, ch4);
+  
+  -- Could read and report a partial string...   Oh well.
+  str = self.f:read(nil, 1024)
+  if str then
+    print("Arduino says: " .. str:match( "^%s*(.-)%s*$" ))
+  end
+end
+
+
 local S = require "syscall"
 
 
-function write(f, a)
+function write(fd, a)
   -- If this assertion fails, it's a buffer overflow on the host side
   -- That likley means the arduino suffered a buffer overflow long ago.
-  assert(f:write(string.char(a/256,a%256)) == 2)
+  assert(fd:write(string.char(a/256,a%256)) == 2)
 end
 
-function sendtoquad(quadnumber, ch1, ch2, ch3, ch4)
-  write(f, quadnumber);
-  write(f, ch1);
-  write(f, ch2);
-  write(f, ch3);
-  write(f, ch4);
-end
+--local fd = open("/dev/ttyUSB0")
 
-function open(device)
-  local f = S.open(device, "rdwr, noctty, nonblock")
-  local f_term = S.tcgetattr(f)
-  f_term:makeraw()
-  f_term.ispeed = 115200
-  f_term.ospeed = 115200
-  f:tcsetattr("now", f_term)
+--for j=1,250 do
+--  sendtoquad(fd, 0, 500, 500, (j%11)*100, 500)
+  
+  
 
-  return f
-
-end
-
-
-local f = open("/dev/ttyUSB0")
-
-for j=1,250 do
-  write(f, 0);
-  write(f, 500);
-  write(f, 500);
-  write(f, (j%11)*100);
-  write(f, 500);
-  str = f:read(nil, 1024)
-  if str then
-    print(str)
-  end
-  os.execute("sleep 0.1")
-end
+--  os.execute("sleep 0.1")
+--end
