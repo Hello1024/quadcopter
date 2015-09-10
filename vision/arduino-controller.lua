5805fac38bb20e303433d5c647516bf7c55c2b99
Oliver Mattos <omattos@gmail.com>
2015-09-10 20:56:07 +0100

    New way to send data from LUA  (fixes requirement to initialize the device to the right mode with the arduino ide first by calling the right syscalls)

--- vision/arduino-controller.lua
+++ vision/arduino-controller.lua
@@ -1,25 +1,50 @@
 #!/usr/bin/env qlua
 
--- sudo  chmod o+rw /dev/ttyU*; sudo stty -F /dev/ttyU* 115200
+-- Depending on your system, you might need to run this:
+-- sudo  chmod o+rw /dev/ttyU*;
 
+
+local S = require "syscall"
 
 
 function write(f, a)
-  f:write(string.char(a/256,a%256))
-  f:flush()
+  -- If this assertion fails, it's a buffer overflow on the host side
+  -- That likley means the arduino suffered a buffer overflow long ago.
+  assert(f:write(string.char(a/256,a%256)) == 2)
+end
+
+function sendtoquad(quadnumber, ch1, ch2, ch3, ch4)
+  write(f, quadnumber);
+  write(f, ch1);
+  write(f, ch2);
+  write(f, ch3);
+  write(f, ch4);
+end
+
+function open(device)
+  local f = S.open(device, "rdwr, noctty, nonblock")
+  local f_term = S.tcgetattr(f)
+  f_term:makeraw()
+  f_term.ispeed = 115200
+  f_term.ospeed = 115200
+  f:tcsetattr("now", f_term)
+
+  return f
+
 end
 
 
-os.execute("stty -F /dev/ttyU* 115200 raw")
+local f = open("/dev/ttyUSB0")
 
-f = assert(io.open("/dev/ttyUSB0", "w"))
-os.execute("sleep 4")
 for j=1,250 do
-   f:write(string.char(255))
-   write(f, 500);
-   write(f, 500);
-   write(f, (j%11)*100);
-   write(f, 500);
-
+  write(f, 0);
+  write(f, 500);
+  write(f, 500);
+  write(f, (j%11)*100);
+  write(f, 500);
+  str = f:read(nil, 1024)
+  if str then
+    print(str)
+  end
   os.execute("sleep 0.1")
 end
