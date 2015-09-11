58ecc9275d817c2490ddbbe7d0deb076648d1ab4
Oliver Mattos <omattos@gmail.com>
2015-09-11 01:08:04 +0100

    tidyup

--- vision/vision-annotate.lua
+++ vision/vision-annotate.lua
@@ -25,24 +25,25 @@ local results = torch.load(file_name..'.annotations', 'ascii')
 local frame = 0
 
 qt.connect(window.listener,
-                 'sigMousePress(int,int,QByteArray,QByteArray,QByteArray)',
-                 function (x,y,...)
-                   
-                   results[frame] = {x=x, y=y}
-                   while results[frame] do
-                     if results[frame].x < 10 and results[frame].y < 10 then
-                       print("moo")
-                       -- No quadcopter in this frame
-                       results[frame].x = -1
-                       results[frame].y = -1
-                     end
-                     frame = frame+1
-                     print(frame)
-                     torch.save(file_name..'.annotations', results , 'ascii')
-                     dd = image.toDisplayTensor{input={cam:forward()},padding=1, scaleeach=true}
-                     image.display{image=dd, win=painter}
-                   end
-                 end)
+   'sigMousePress(int,int,QByteArray,QByteArray,QByteArray)',
+   function (x,y,...)
+     
+     results[frame] = {x=x, y=y}
+     while results[frame] do
+       if results[frame].x < 10 and results[frame].y < 10 then
+         print("moo")
+         -- No quadcopter in this frame
+         results[frame].x = -1
+         results[frame].y = -1
+       end
+       frame = frame+1
+       print(frame)
+       torch.save(file_name..'.annotations', results , 'ascii')
+       dd = image.toDisplayTensor{input={cam:forward()},padding=1, scaleeach=true}
+       image.display{image=dd, win=painter}
+     end
+   end)
+
 window:show()
 
 
